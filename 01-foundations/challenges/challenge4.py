7cb270f1962f946b61d4f04afd78cece624e3f91
Muktar Yakub <rmubez@gmail.com>
2026-07-11 17:25:34 +0200

    Add greeting message functionality in challenge4.py

---
+++ 01-foundations/challenges/challenge4.py
@@ -0,0 +1,8 @@
+name = input("what is your name :")
+friends_name=input("whats is your friends name :")
+
+Message = "Wishing you the best!"
+
+print (f"To: {friends_name}")
+print (f"From: {name}".upper())
+print (f"Message {Message}")
