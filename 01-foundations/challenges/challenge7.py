8357ea6c14e26fa0fe5bbc70347162c1224adbad
Muktar Yakub <rmubez@gmail.com>
2026-07-11 17:28:04 +0200

    Add user input for name and birth year

---
+++ 01-foundations/challenges/challenge7.py
@@ -0,0 +1,6 @@
+first_name= input("What is your first name? ")
+birth_year= input("What is your birth year? ")
+
+print(f"Name: {first_name.capitalize()}")
+print(f"Year: {birth_year}")
+print(f"Username: {first_name.lower()[0:4]}{birth_year}")
