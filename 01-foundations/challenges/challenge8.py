33399a76c25c805cc07e3d6b6d13ab2b36b3bc8c
Muktar Yakub <rmubez@gmail.com>
2026-07-11 17:29:00 +0200

    Add coffee order input and total calculation

---
+++ 01-foundations/challenges/challenge8.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+coffees= int(input("How many coffees? "))
+price_coffees=5
+
+print(f"Customer: {name.upper()}")
+
+total= coffees*price_coffees
+
+print(f"Coffees: {coffees}")
+print(f"Total: ${total}")
