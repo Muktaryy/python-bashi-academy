a0cdad1c7fd4894cbcc53b4f5cc2674cb5c4be4f
Muktar Yakub <rmubez@gmail.com>
2026-07-11 17:29:32 +0200

    Add user input for name, phone, and email

---
+++ 01-foundations/challenges/challenge9.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+phone = int(input("What is your phone? "))
+email= input("What is your email? ")
+
+print(f"Name: {name.capitalize()}")
+print(f"Phone: {phone}")
+print(f"Email: {email}({(len(email))} charecters)" )
