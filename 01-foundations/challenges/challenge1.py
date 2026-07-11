630f7f9647796b34c181b2d501c8f4fd1ff335d0
Muktar Yakub <rmubez@gmail.com>
2026-07-11 17:22:55 +0200

    Add basic budget calculation in challenge1.py

---
+++ 01-foundations/challenges/challenge1.py
@@ -0,0 +1,15 @@
+rice_price =25
+chicken_price = 40
+bread_price= 8
+milk_price=6
+total= rice_price+chicken_price+bread_price+milk_price
+#print(total)
+
+budget=200
+change= budget-total
+#print(change)
+
+#f"string"
+print(f"Total: {total}")
+print(f"Budget: {budget}")
+print(f"Change: {change}")
