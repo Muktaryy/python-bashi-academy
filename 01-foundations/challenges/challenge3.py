8e4e60cb438761a33048afd048fbb681ce10648c
Muktar Yakub <rmubez@gmail.com>
2026-07-11 17:24:46 +0200

    Implement flight booking fee calculation
    Calculate flight booking fee and total cost.

---
+++ 01-foundations/challenges/challenge3.py
@@ -0,0 +1,12 @@
+booking_flight =500
+booking_percentage =10/100
+fee = booking_flight * booking_percentage
+#print(fee)
+
+total = booking_flight + fee
+#print (total)
+
+#fstrings
+print(f"Flight :{booking_flight}")
+print(f"Fee: {fee}")
+print(f"Total: {total}")
