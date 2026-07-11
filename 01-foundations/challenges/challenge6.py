aeda2d4a289b7a6bce02b573bed3f537b710ebb6
Muktar Yakub <rmubez@gmail.com>
2026-07-11 17:27:29 +0200

    Add bill splitting functionality in challenge6.py

---
+++ 01-foundations/challenges/challenge6.py
@@ -0,0 +1,8 @@
+total = float(input("How much total bill? "))
+people = int(input("How many people? "))
+
+print(f"Total bill: {total}")
+
+each = total/people
+print(f"Friends= {people}")
+print(f"Each pays: {each}")
