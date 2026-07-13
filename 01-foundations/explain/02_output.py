ab0d0ce3d2840649e4c33e87a972008fbc1a76b9
Muktar Yakub <rmubez@gmail.com>
2026-07-13 11:25:29 +0200

    Add print output examples in 02_output.py
    Added examples demonstrating the use of print() in Python for strings, numbers, and formatting with new lines and tab spaces.

---
+++ 01-foundations/explain/02_output.py
@@ -0,0 +1,57 @@
+# 🖨️ Print Output
+
+
+# print() is used to display information on the screen.
+# It can show text, numbers, and multiple values.
+
+
+# Printing text (String)
+
+print("Hello Python")
+
+
+# Output:
+# Hello Python
+
+
+# Printing numbers
+
+print(25)
+print(3.99)
+
+
+# Output:
+# 25
+# 3.99
+
+
+# Printing multiple values
+
+print("Name:", "Ahmed")
+print("Age:", 20)
+
+
+# Output:
+# Name: Ahmed
+# Age: 20
+
+
+# New line (\n)
+# \n moves the next text to a new line.
+
+print("Hello\nWorld")
+
+
+# Output:
+# Hello
+# World
+
+
+# Tab space (\t)
+# \t adds a tab space.
+
+print("Name:\tAhmed")
+
+
+# Output:
+# Name:    Ahmed
