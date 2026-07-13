f9f8016ef706ab23a0f36d1e2849fbef154d4ffd
Muktar Yakub <rmubez@gmail.com>
2026-07-13 10:39:50 +0200

    Add grading logic based on score thresholds

---
+++ 02-logic/challenges/challenge6.py
@@ -0,0 +1,16 @@
+if (score >=90):
+    print(f"Score: {score}")
+    print(f"Grade: A")
+
+elif(score >=80):
+    print(f"Score: {score}")
+    print(f"Grade: B")
+elif(score>=70):
+    print(f"Score: {score}")
+    print(f"Grade: c")
+elif(score>=60):
+    print(f"Score: {score}")
+    print(f"Grade: D")
+elif(score<60):
+    print(f"Score: {score}")
+    print(f"Grade: F")
