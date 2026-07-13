ddac941c0d6fdae304cbaaf77b4403d49b171f0d
Muktar Yakub <rmubez@gmail.com>
2026-07-13 10:39:04 +0200

    Add parking fee calculation based on hours

---
+++ 02-logic/challenges/challenge3.py
@@ -0,0 +1,10 @@
+hour = int(input("How many hours are you parking? "))
+if (hour ==1):
+    print(f"Hours: {hour}")
+    print(f"Fee: $5")
+elif (hour >=4):
+    print(f"Hours: {hour}")
+    print(f"Fee: $20")
+elif(hour >=2):
+    print(f"Hours: {hour}")
+    print(f"Fee: $10")
