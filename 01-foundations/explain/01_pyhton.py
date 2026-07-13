68ab801c4a5812684230ddedabc1291d0b5e66b6
Muktar Yakub <rmubez@gmail.com>
2026-07-13 11:22:45 +0200

    Add introductory Python explanation and example

---
+++ 01-foundations/explain/01_pyhton.py
@@ -0,0 +1,19 @@
+# 🐍 What is Python?
+
+
+# Python is a programming language.
+# It is used to create websites, apps, AI projects,
+# automation tools, and work with data.
+#
+# Python is popular because it is simple,
+# easy to read, and powerful.
+
+
+# 🚀 My First Python Code
+
+
+print("Hello World")
+
+
+# Output:
+# Hello World
