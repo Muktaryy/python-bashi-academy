3a45bb593c058e7a2292a331eb97916d5442d720
Muktar Yakub <rmubez@gmail.com>
2026-07-13 15:24:22 +0200

    Format bill and payment amounts to two decimal places

--- 01-foundations/challenges/challenge6.py
+++ 01-foundations/challenges/challenge6.py
@@ -1,8 +1,8 @@
 total = float(input("How much total bill? "))
 people = int(input("How many people? "))
 
-print(f"Total bill: {total}")
+print(f"Total bill: {total:.2f}")
 
 each = total/people
 print(f"Friends= {people}")
-print(f"Each pays: {each}")
+print(f"Each pays: {each:.2f}")
