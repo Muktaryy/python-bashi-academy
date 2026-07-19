33964a6deed0cca9dcf590a2065d7c72e11c794b
Muktar Yakub <rmubez@gmail.com>
2026-07-19 02:53:29 +0200

    Add person dictionary with details and print statements

---
+++ 04-collections/challenges/challenge6.py
@@ -0,0 +1,11 @@
+person= { 
+  "name": "Ahmed", 
+  "age" : 22,
+ "city" : "Oslo"}
+person["city"]= "London"
+person["email"]="ahmed@email.com"
+
+print(f"Name: {person['name']}" )
+print(f"Age: {person['age']}" )
+print(f"City: {person['city']}")
+print(f"Email: {person['email']}")
