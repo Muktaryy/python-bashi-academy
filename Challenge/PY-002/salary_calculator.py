03498f4aa85b676ed7a21e3a4b7349b2da2c84da
Muktar Yakub <rmubez@gmail.com>
2026-08-23 01:35:18 +0200

    Add salary calculator to compute monthly and annual salary

---
+++ Challenge/PY-002/salary_calculator.py
@@ -0,0 +1,7 @@
+monthly_salary = 3500
+annual_salary = monthly_salary * 12
+
+print("Salary Information")
+print("------------------")
+print(f"Monthly Salary: {monthly_salary}")
+print(f"Annual Salary: {annual_salary}")
