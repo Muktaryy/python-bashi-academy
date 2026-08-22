1800767013feaac01aa5f597878cfbe5658b3744
Muktar Yakub <rmubez@gmail.com>
2026-08-23 01:33:53 +0200

    Add employee information display in employee.py

---
+++ Challenge/PY-001/employee.py
@@ -0,0 +1,13 @@
+name = "Ahmed"
+age = 25
+job_title = "Python Developer"
+salary = 35000
+is_active = True
+
+print("Employee Information")
+print("--------------------")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Job Title: {job_title}")
+print(f"Salary: {salary}")
+print(f"Active: {is_active}")
