090b85e1cbbd41c26701d22f4965288c4a6057b4
Muktar Yakub <rmubez@gmail.com>
2026-08-26 10:33:53 +0200

    Add employee promotion eligibility check

---
+++ Challenge/PY-006/employee_promotion.py
@@ -0,0 +1,7 @@
+age = 30
+years_at_company = 2
+
+if age >= 25 and years_at_company >= 2:
+    print("Employee is eligible for promotion.")
+else:
+    print("Employee is not eligible for promotion.")
