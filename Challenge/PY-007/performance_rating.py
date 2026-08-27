35ba9b18395e4d20c4b207333aa98a6d9d8c0d9f
Muktar Yakub <rmubez@gmail.com>
2026-08-27 23:38:52 +0200

    Add performance rating script based on score

---
+++ Challenge/PY-007/performance_rating.py
@@ -0,0 +1,10 @@
+score = 85
+
+if score >= 90:
+    print("Performance Rating: Excellent")
+elif score >= 75:
+    print("Performance Rating: Good")
+elif score >= 60:
+    print("Performance Rating: Satisfactory")
+else:
+    print("Performance Rating: Needs Improvement")
